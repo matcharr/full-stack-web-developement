ba4e1dd28678cbd2b8a767bd558bb3b1aa2ed686
kyo <mathis.charretier.42@gmail.com>
2022-06-12 20:57:04 +0000

    update

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -2,13 +2,13 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { getRequest } from '@sveltejs/kit/node';
 import { api } from './_api';
 
-export const del: RequestHandler = (request) => {
-	return api(request);
+export const del: RequestHandler = (request, data) => {
+	return api(request, data);
 };
 
-export const patch: RequestHandler<{}, FormData> = (request) => {
+export const patch: RequestHandler<{}, FormData> = (request, data) => {
 	return api(request, {
-		text: request.body.get('text')
+		text: request.body.get('text'),
 		done: request.body.has('done') ? !!request.body.get('done') : undefined
 	});
 };
--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -1,11 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { api } from './_api';
 
-export const get: RequestHandler = (request) => {
-	return api(request);
+export const get: RequestHandler = (request, data) => {
+	return api(request, data);
 };
 
-export const post: RequestHandler<{}, FormData> = (request) => {
+export const post: RequestHandler<{}, FormData> = (request, data) => {
 	return api(request, {
 		created_at: new Date(),
 		text: request.body.get('text'),
